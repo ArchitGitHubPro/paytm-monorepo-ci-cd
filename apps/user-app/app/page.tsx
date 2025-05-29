59627c935e60965eade9cd92eece47378e74a908
ArchitGitHubPro <archit.7537@gmail.com>
2025-05-30 00:15:21 +0530

    updated navbar

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,6 +2,7 @@
 import { PrismaClient } from "@repo/db/client";
 import { useBalance } from "@repo/store/balance";
 import { Appbar } from "@repo/ui/Appbar"
+import { signIn, signOut, useSession } from "next-auth/react";
 
 
 const prisma = new PrismaClient();
@@ -9,10 +10,11 @@ const prisma = new PrismaClient();
 
 export default function Home() {
   const balance = useBalance();
+  const session = useSession();
 
   return (
     <div className="bg-red-200">
-      {/* <Appbar  onSignin={signIn} onSignout={signOut} user={session.data?.user}/> */}
+      <Appbar  onSignIn={signIn} onSignOut={signOut} user={session.data?.user}/>
       Hi there {balance}
     </div>
   );
--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,21 +1,26 @@
+import { Button } from "./button"
+
     
 interface AppbarProps {
     user?:{
         name?: string | null 
     },
-        onSignin: () => void
-        onSignout: () => void
+        onSignIn: () => void
+        onSignOut: () => void
 }
 
 export const Appbar = ({
     user,
-    onSignin,
-    onSignout 
+    onSignIn,
+    onSignOut 
 }: AppbarProps) => {
 
     return <div className="flex justify-between border-b px-4 border-slate-300">
         <div className="text-lg flex flex-col justify-center">
             Paytm
+        </div>
+        <div className="flex flex-col justify-center pt-2">
+            <Button onClick = { user ? onSignOut: onSignIn}> {user ? 'Logout': 'Login'} </Button>
         </div>
     </div>
 }
